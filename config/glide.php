a08a48c0c331743bb4c2f8b75661c7df74aa77aa
Ralph J. Smit <rjs@ralphjsmit.com>
2026-03-14 16:52:11 +0100

    WIP

--- config/glide.php
+++ config/glide.php
@@ -1,6 +1,14 @@
 <?php
 
 return [
+    /**
+     * The default filesystem disk to read source images from.
+     * Set to `null` to use the `public/` directory, or specify
+     * a disk name like `public` or `s3` to read from
+     * a configured filesystem disk instead.
+     */
+    'default_source_disk' => null,
+
     'route' => [
         /**
          * The domain that will be used to generate the Glide URLs.
--- src/GlideImageGenerator.php
+++ src/GlideImageGenerator.php
@@ -3,55 +3,65 @@
 namespace RalphJSmit\Laravel\Glide;
 
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Str;
 use Illuminate\View\ComponentAttributeBag;
 use Intervention\Image\ImageManager;
+use League\Flysystem\Filesystem;
+use League\Flysystem\FilesystemOperator;
+use League\Flysystem\Local\LocalFilesystemAdapter;
 use RuntimeException;
 
 class GlideImageGenerator
 {
-    public function src(string $path, ?int $maxWidth = null, ?string $sizes = null, bool $lazy = true, bool $grow = false): ComponentAttributeBag
+    public function src(string $path, ?int $maxWidth = null, ?string $sizes = null, bool $lazy = true, bool $grow = false, ?string $disk = null): ComponentAttributeBag
     {
         $attributes = new ComponentAttributeBag();
 
         $isGlideSupported = $this->isGlideSupported($path);
 
         $attributes->setAttributes([
-            'src' => $this->getSrcAttribute($path, $maxWidth),
-            ...$isGlideSupported ? ['srcset' => $this->getSrcsetAttribute($path, $maxWidth)] : [],
+            'src' => $this->getSrcAttribute($path, $maxWidth, $disk),
+            ...$isGlideSupported ? ['srcset' => $this->getSrcsetAttribute($path, $maxWidth, $disk)] : [],
             ...($isGlideSupported && $sizes !== null) ? ['sizes' => $sizes] : [],
             ...$lazy ? ['loading' => 'lazy'] : [],
         ]);
 
         if (! $grow) {
-            $attributes = $attributes->style("max-width: {$this->getImageWidth($path)}px");
+            $attributes = $attributes->style("max-width: {$this->getImageWidth($path, $disk)}px");
         }
 
         return $attributes;
     }
 
-    protected function getSrcAttribute(string $path, ?int $maxWidth): string
+    protected function getSrcAttribute(string $path, ?int $maxWidth, ?string $disk = null): string
     {
+        $resolvedDisk = $disk ?? config('glide.default_source_disk');
+
         if (! $this->isGlideSupported($path)) {
-            return asset($path);
+            return $resolvedDisk
+                ? Storage::disk($resolvedDisk)->url($path)
+                : asset($path);
         }
 
         if ($maxWidth === null) {
-            return asset($path);
+            return $resolvedDisk
+                ? $this->generateUrl($path, [], $disk)
+                : asset($path);
         }
 
-        $imageWidth = $this->getImageWidth($path);
+        $imageWidth = $this->getImageWidth($path, $disk);
 
         // For generating the `src` url, we should not use values bigger than the image width, because
         // the browser will load these images at their original size as second request after picking
         // the optimal version. An upsized version should be a convenience thing and not a default.
         return $this->generateUrl($path, [
             'width' => $imageWidth ? min($imageWidth, $maxWidth) : $maxWidth,
-        ]);
+        ], $disk);
     }
 
-    protected function getSrcsetAttribute(string $path, ?int $maxWidth): string
+    protected function getSrcsetAttribute(string $path, ?int $maxWidth, ?string $disk = null): string
     {
         $scale = collect([
             400,
@@ -71,7 +81,7 @@ class GlideImageGenerator
             10000,
         ]);
 
-        $imageWidth = $this->getImageWidth($path);
+        $imageWidth = $this->getImageWidth($path, $disk);
 
         $scale = $scale
             ->when($maxWidth)->reject(fn (int $width) => $width > $maxWidth)
@@ -85,25 +95,39 @@ class GlideImageGenerator
         }
 
         return $scale
-            ->mapWithKeys(function (int $width) use ($path): array {
-                return [$width => $this->generateUrl($path, ['width' => $width])];
+            ->mapWithKeys(function (int $width) use ($path, $disk): array {
+                return [$width => $this->generateUrl($path, ['width' => $width], $disk)];
             })
             ->map(fn (string $src, int $width) => "{$src} {$width}w")
             ->implode(', ');
     }
 
-    protected function getImageWidth(string $path): ?int
+    protected function getImageWidth(string $path, ?string $disk = null): ?int
     {
-        return Cache::rememberForever("glide::image-generator.image-width.{$path}", function () use ($path) {
-            return rescue(fn () => $this->getImageManager()->read(public_path($path))->width());
+        $cacheKey = "glide::image-generator.image-width.{$disk}.{$path}";
+
+        return Cache::rememberForever($cacheKey, function () use ($path, $disk) {
+            return rescue(fn () => $this->getImageManager()->read(
+                $this->getSourceFilesystem($disk)->read($path)
+            )->width());
         });
     }
 
-    protected function generateUrl(string $path, array $parameters): string
+    protected function generateUrl(string $path, array $parameters, ?string $disk = null): string
     {
+        if ($disk && ! config('glide.route.signed')) {
+            throw new RuntimeException('Disk parameter requires signed URLs to be enabled in glide config.');
+        }
+
+        $params = ['source' => $path, ...$parameters];
+
+        if ($disk) {
+            $params['disk'] = $disk;
+        }
+
         return config('glide.route.signed')
-            ? Url::signedRoute('glide.generate', ['source' => $path, ...$parameters])
-            : route('glide.generate', ['source' => $path, ...$parameters]);
+            ? Url::signedRoute('glide.generate', $params)
+            : route('glide.generate', $params);
     }
 
     protected function isGlideSupported(string $path): bool
@@ -124,9 +148,17 @@ class GlideImageGenerator
         throw new RuntimeException('No supported image driver (GD or Imagick) is installed.');
     }
 
-    public function getSourcePath(): string
+    public function getSourceFilesystem(?string $disk = null): FilesystemOperator
     {
-        return public_path();
+        $disk ??= config('glide.default_source_disk');
+
+        if (! $disk) {
+            return new Filesystem(
+                new LocalFilesystemAdapter(public_path())
+            );
+        }
+
+        return Storage::disk($disk)->getDriver();
     }
 
     public function getCachePath(): string
