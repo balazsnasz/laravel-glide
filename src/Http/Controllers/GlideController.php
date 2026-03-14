b6472a2bf5225ffc09d5925a5c0c1b84446fb88e
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2026-03-14 20:37:44 +0100

    feat: disk support (#25)
    * WIP
    
    * style: apply Pint

--- src/Http/Controllers/GlideController.php
+++ src/Http/Controllers/GlideController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use League\Glide\Filesystem\FileNotFoundException;
 use League\Glide\ServerFactory;
 use RalphJSmit\Laravel\Glide\Http\Controllers\GlideController\GlideResponseFactory;
+use RuntimeException;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class GlideController
@@ -15,10 +16,15 @@ class GlideController
     public function __invoke(Request $request, Application $application, Filesystem $filesystem, string $domainOrSource, ?string $source = null): StreamedResponse
     {
         $source = $source ?? $domainOrSource;
+        $disk = $request->query('disk');
+
+        if ($disk && ! config('glide.route.signed')) {
+            throw new RuntimeException('Disk parameter requires signed URLs to be enabled in glide config.');
+        }
 
         $server = ServerFactory::create([
             'response' => new GlideResponseFactory($request),
-            'source' => glide()->getSourcePath(),
+            'source' => glide()->getSourceFilesystem($disk),
             'cache' => glide()->getCachePath(),
             'base_url' => '',
         ]);
